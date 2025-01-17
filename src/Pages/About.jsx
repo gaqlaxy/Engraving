c611e4a10138bd7f0768a9b092b82281d9de64cb
gaqlaxy <v4vripper@gmail.com>
2025-01-17 06:49:51 +0530

    Started About Section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,14 +3,33 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <div className="aboutus">
-        <h1>About us</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          minima consectetur mollitia corporis exercitationem, doloribus ab
-          sequi cum blanditiis harum iste quod quia optio fugiat repellendus
-          nulla praesentium esse quos.
-        </p>
+      <div className="aboutus flex justify-between relative ">
+        <div className="aboutus-content">
+          <h1>About us</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            minima consectetur mollitia corporis exercitationem, doloribus ab
+            sequi cum blanditiis harum iste quod quia optio fugiat repellendus
+            nulla praesentium esse quos.
+          </p>
+        </div>
+        <div className="aboutus-img relative">
+          <img
+            src="heroimg2.jpg"
+            alt=""
+            className="w-1/2 absolute top-[10.5rem] right-[11rem] z-10"
+          />
+          <img
+            src="heroimg3.jpg"
+            alt=""
+            className="w-1/2 absolue top-1 right-2 z-10"
+          />
+          <img
+            src="heroimg1.jpg"
+            alt=""
+            className="w-1/2 absolute right-[2rem] top-[1.25rem] z-0 "
+          />
+        </div>
       </div>
     </>
   );
